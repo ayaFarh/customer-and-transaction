a92e54e004711f3e793bcf3b5e219a3752a04fb4
ayaFarh <farhaya004@gmail.com>
2024-07-14 18:29:21 +0300

    Move db.json to public and update fetch path

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/customer-and-transaction",
+  base: '/customer-and-transaction/',
   server: {
     fs: {
       strict: false
